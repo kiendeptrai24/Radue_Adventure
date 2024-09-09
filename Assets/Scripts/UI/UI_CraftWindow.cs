9b6104db55c86c51150ebcb7a276884d26fe2e9f
kiendeptrai24 <kienmiu2004@gmail.com>
2024-09-09 23:03:08 +0700

    clean class UI_CraftWindow

--- Assets/Scripts/UI/UI_CraftWindow.cs
+++ Assets/Scripts/UI/UI_CraftWindow.cs
@@ -17,36 +17,43 @@ public class UI_CraftWindow : MonoBehaviour
     public void SetupCraftWindow(ItemData_Equipment _data)
     {
         craftButton.onClick.RemoveAllListeners();
+        
+        ClearCraftElement();
 
+        for (int i = 0; i < _data.craftingMaterial.Count; i++)
+        {
+            AssignInfoForCraftElement(_data, i);
+        }
+        
+        itemIcon.sprite = _data.itemIcon;
+        itemName.text = _data.itemName;
+        itemDescription.text = _data.GetDescription();
 
+        craftButton.onClick.AddListener(() => Inventory.instance.CanCraft(_data, _data.craftingMaterial));
+    }
+
+    private void ClearCraftElement()
+    {
         for (int i = 0; i < materialImage.Length; i++)
         {
             materialImage[i].color = Color.clear;
             materialImage[i].GetComponentInChildren<TextMeshProUGUI>().color = Color.clear;
 
         }
-        for (int i = 0; i < _data.craftingMaterial.Count; i++)
-        {
-            if(_data.craftingMaterial.Count >materialImage.Length)
-                Debug.LogWarning("You have more materials amount than you have material slots in craft window");
-
-            materialImage[i].sprite =_data.craftingMaterial[i].data.itemIcon;
-            materialImage[i].color =Color.white;
-
-            TextMeshProUGUI materialSlotText =materialImage[i].GetComponentInChildren<TextMeshProUGUI>();
-
-            materialSlotText.text = _data.craftingMaterial[i].stackSize.ToString();
-
-            materialSlotText.color = Color.white;
-        }
-
-        itemIcon.sprite = _data.itemIcon;
-        itemName.text = _data.itemName;
-        itemDescription.text = _data.GetDescription();
-
-        craftButton.onClick.AddListener(() => Inventory.instance.CanCraft(_data,_data.craftingMaterial));
     }
 
+    private void AssignInfoForCraftElement(ItemData_Equipment _data, int i)
+    {
+        if (_data.craftingMaterial.Count > materialImage.Length)
+            Debug.LogWarning("You have more materials amount than you have material slots in craft window");
 
+        materialImage[i].sprite = _data.craftingMaterial[i].data.itemIcon;
+        materialImage[i].color = Color.white;
 
+        TextMeshProUGUI materialSlotText = materialImage[i].GetComponentInChildren<TextMeshProUGUI>();
+
+        materialSlotText.text = _data.craftingMaterial[i].stackSize.ToString();
+
+        materialSlotText.color = Color.white;
+    }
 }
